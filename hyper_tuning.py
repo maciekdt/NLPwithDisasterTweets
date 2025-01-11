98f2d594d1e8b451f5b46c5e53b751d97a8b1bba
maciekdt <74464525+maciekdt@users.noreply.github.com>
2025-01-11 17:42:25 +0100

    ...

--- hyper_tuning.py
+++ hyper_tuning.py
@@ -12,7 +12,7 @@ args = parser.parse_args()
 def optimize_bert(trial):
     lr = trial.suggest_float('lr', 1e-6, 1e-1, log=True)
     lr_pre = trial.suggest_float('lr_pre', 1e-6, 1e-1, log=True)
-    batch_size = trial.suggest_int("batch_size", 16, 512, log=True)
+    batch_size = trial.suggest_int("batch_size", 16, 32, log=True)
     model_id = trial.suggest_categorical('model_id', ['bert-large-uncased'])#['bert-base-uncased', 'bert-large-uncased'])
     
     trainer = CustomTrainer()
